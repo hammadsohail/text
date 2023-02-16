7b354099becdac343cd27961a9a0030d61cce9d7
hammadsohail <muhammadhammad848@gmail.com>
2023-02-16 13:14:42 +0500

    message01

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Typography, Link } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { createTheme } from '@mui/material/styles';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const theme = createTheme();
 
@@ -29,57 +30,81 @@ export function DesktopNavbar() {
   return (
     <>
     {/* Top Nav 01 */}
-      <div className="flex flex-row justify-between top-0 bottom-auto px-4 py-6	 bg-[#FEEE00] z-60 space-x-4">
+      <div className="flex flex-row justify-between top-0 bottom-auto px-4 bg-[#FEEE00] z-60 space-x-9 h-24 items-center ">
         <img className="h-9 " src="https://z.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" />
 
-        <button className="flex flex-row space-x-4">
+        <button className="flex flex-row space-x-4 items-center ml-6 h-10 hover:opacity-60">
           <img src="https://z.nooncdn.com/s/app/com/common/images/flags/ae.svg" alt="country-ae" className="h-8" />
-          <div className="-mt-4 flex-col space-y-2">
-            <span className="flex flex-row text-2xl color-gray-400">
+
+            <span className="flex flex-col text-2xl color-gray-400">
+              <span className="flex inline-flex align-baseline">
               Deliver to
-              <div className="ml-6">
-                <img className="h-8" src="https://z.nooncdn.com/s/app/com/noon/icons/dropdownArrow.svg" />
+              <div className="w-6 h-6">
+                <img className="ml-3 mr-3 w-full h-full" src="https://z.nooncdn.com/s/app/com/noon/icons/dropdownArrow.svg" />
               </div>
-            </span>
-            <div className="flex justify-start">
-              <span className="font-bold text-xl">Dubai</span>
-            </div>
+              </span>
+
+
+
+
+            <div className="flex flex-row items-center ">
+              <span className="font-bold text-xl inline-flex text-ellipsis overflow-hidden	whitespace-nowrap	">Dubai</span>
+
           </div>
+
+          </span>
         </button>
 
         <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer ">
           <input className=" p-3 flex-grow flex-shrink bg-white rounded-lg text-2xl" placeholder="What are you looking for ?" />
         </div>
 
-        <div className="flex flex-row space-x-1 items-center">
-          <div className="text-2xl border-r border-gray-400 pr-4">العربية</div>
-          <button className="flex flex-row text-2xl font-bold border-r border-gray-400 pr-4">
-            Sign In <img className="ml-4 h-8" src="https://z.nooncdn.com/s/app/com/noon/icons/user_thin.svg" />
-          </button>
-          <button className="flex flex-row text-2xl font-bold pr-4 ">
-            Cart <img className="ml-4 h-8" src="https://z.nooncdn.com/s/app/com/noon/icons/cart.svg" />
-          </button>
+        <div className="flex items-center">
+          <div className="text-2xl">العربية</div>
+          <div className="flex mx-4 border-l border-gray-400 h-6"></div>
         </div>
+
+        <div className="">
+          <div className="relative">
+
+             <button className="flex flex-row items-center justify-center">
+               <span className="font-bold text-right text-2xl"> Sign In </span>
+             <img className="ml-4 w-8" src="https://z.nooncdn.com/s/app/com/noon/icons/user_thin.svg" />
+
+             </button>
+
+          </div>
+        </div>
+
+          <button className="flex items-center px-5 border-l border-gray-400 hover:opacity-60 ">
+            <span className="flex mr-3 text-2xl font-bold">Cart</span>
+            <div className="flex relative">
+
+             <img className="h-8 w-8" src="https://z.nooncdn.com/s/app/com/noon/icons/cart.svg" />
+             </div>
+          </button>
+
       </div>
 
       {/* top Nav 02 (list) */}
 
-      <div className="flex space-x-8 items-center justify-between px-4 h-16 bg-white flex-row w-full">
+      <div className="flex space-x-8 items-center justify-items p-6 h-16 bg-[#FCFBF4] flex-row w-full">
 
-       <div>
-        <h1 className="text-md text-blue-600 font-bold">ALL CATEGORIES</h1>
+       <div className="flex-grow flex flex-row justify-between items-center">
+        <h1 className="text-xl text-blue-600 font-bold">ALL CATEGORIES</h1>
+        <KeyboardArrowDownIcon size={50} className="text-blue-400 h-20 w-20 border-r border-gray-400 h-14" />
        </div>
 
-       <div className="flex flex-row space-x-5">
-       <h1 className="text-md color-blue-400 font-bold">ELECTRONICS</h1>
-       <h1 className="text-md color-blue-400 font-bold">MEN</h1>
-       <h1 className="text-md color-blue-400 font-bold">WOMEN</h1>
-       <h1 className="text-md color-blue-400 font-bold">HOME</h1>
-       <h1 className="text-md color-blue-400 font-bold">BEAUTY & FRAGRANCE</h1>
-       <h1 className="text-md color-blue-400 font-bold">ALL CATEGORIES</h1>
-       <h1 className="text-md color-blue-400 font-bold">ALL CATEGORIES</h1>
-       <h1 className="text-md color-blue-400 font-bold">ALL CATEGORIES</h1>
-       <h1 className="text-md color-blue-400 font-bold">ALL CATEGORIES</h1>
+       <div className="flex flex-row space-x-8">
+       <h1 className="text-xl  color-blue-400 font-bold">ELECTRONICS</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">MEN</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">WOMEN</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">HOME</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">BEAUTY & FRAGRANCE</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">ALL CATEGORIES</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">ALL CATEGORIES</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">ALL CATEGORIES</h1>
+       <h1 className="text-xl  color-blue-400 font-bold">ALL CATEGORIES</h1>
 
        </div>
 
